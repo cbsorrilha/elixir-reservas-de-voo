67b121c052ccaaf2be2e962cfe8c54ead6d83b43
Cesar de Barros <cesar.de.b.sorrilha@accenture.com>
2021-07-06 14:48:04 -0300

    feat: permite gerar relatórios em range de data

--- lib/bookings/agent.ex
+++ lib/bookings/agent.ex
@@ -14,6 +14,20 @@ defmodule Flightex.Bookings.Agent do
 
   def list_all, do: Agent.get(__MODULE__, & &1)
 
+  def list_between_dates(from_date, to_date) do
+    Agent.get(__MODULE__, & &1)
+    |> Map.values()
+    |> Enum.filter(fn booking -> compare_date(booking, from_date, to_date) end)
+  end
+
+  defp compare_date(%Booking{complete_date: complete_date}, from_date, to_date) do
+
+    (NaiveDateTime.compare(complete_date, from_date) == :eq ||
+    NaiveDateTime.compare(complete_date, from_date) == :gt) &&
+    (NaiveDateTime.compare(complete_date, to_date) == :eq ||
+    NaiveDateTime.compare(complete_date, to_date) == :lt)
+  end
+
   defp update_state(state, id, %Booking{} = booking), do: Map.put(state, id, booking)
 
   def get(id), do: Agent.get(__MODULE__, &get_booking(&1, id))
--- lib/bookings/report.ex
+++ lib/bookings/report.ex
@@ -3,14 +3,26 @@ defmodule Flightex.Bookings.Report do
   alias Flightex.Bookings.Booking
 
   def generate(filename \\ "report.csv") do
-    booking_list = build_booking_list()
+    booking_list = BookingAgent.list_all()
+      |> Map.values()
+      |> build_booking_list()
 
     File.write(filename, booking_list)
   end
 
-  defp build_booking_list do
-    BookingAgent.list_all()
-    |> Map.values()
+  def generate_report(from_date, to_date) do
+    list = BookingAgent.list_between_dates(from_date, to_date)
+    booking_list = build_booking_list(list)
+
+    File.write(create_file_name(from_date, to_date), booking_list)
+  end
+
+  defp create_file_name(from_date, to_date) do
+    "report#{NaiveDateTime.to_date(from_date)}to#{NaiveDateTime.to_date(to_date)}.csv"
+  end
+
+  defp build_booking_list(list) do
+    list
     |> Enum.map(fn booking -> make_booking_line(booking) end)
   end
 
@@ -20,6 +32,6 @@ defmodule Flightex.Bookings.Report do
          local_destination: local_destination,
          user_id: user_id
        }) do
-    "#{user_id}, #{local_origin}, #{local_destination},#{complete_date}"
+    "#{user_id},#{local_origin},#{local_destination},#{complete_date}\n"
   end
 end
